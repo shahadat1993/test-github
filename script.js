6ce9b2d6707f3c458bb680dd83b80b0791e4a305
Shahahdat <s0735949@gmail.com>
2025-05-05 13:15:23 -0700

    initaial stage 0

---
+++ script.js
@@ -0,0 +1,16 @@
+// ddjfkdjfkl;
+
+// djfj
+// hi i'm here to code
+
+// there has a line also
+// ddjfkdjfkl;
+
+// djfj
+// hi i'm here to code
+
+// me to added one more line
+// hi what are you doing
+// me coding
+var a = 10;
+var b = 12;
